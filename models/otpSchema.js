cdf27001b93f6703518e1d222d784b7ab4bc92b2
deepakgeekmaster <deepakgeekmaster@gmail.com>
2024-09-03 15:43:09 +0530

    Create otpSchema.js

---
+++ models/otpSchema.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const otpSchema = new mongoose.Schema({
+    email: String,
+    otp: String,
+    createdAt: { type: Date, expires: '5m', default: Date.now }
+});
+
+module.exports = mongoose.model('Otp', otpSchema);
