d13983cc44b8b612d647897f859ceabf8369c3b8
deepakgeekmaster <deepakgeekmaster@gmail.com>
2024-09-03 15:32:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(session({
         mongoUrl: process.env.MONGO_URI, 
         collectionName: 'sessions' 
     }),
-    cookie: { secure: true }
+    cookie: { secure: true, httpOnly: false, maxAge: 24 * 60 * 60 * 1000 }
 }));
   const store = new MongoStore({
     mongoUrl: process.env.MONGO_URI,
