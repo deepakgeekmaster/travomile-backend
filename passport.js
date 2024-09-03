f57868c46dbdbb14932c80707330760de87c870c
deepakgeekmaster <deepakgeekmaster@gmail.com>
2024-09-03 10:36:56 +0530

    Update passport.js

--- passport.js
+++ passport.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'travomile-backend.vercel.app/auth/google/callback'
+    callbackURL: 'https://travomile-backend.vercel.app/auth/google/callback'
 
 }, async (accessToken, refreshToken, profile, done) => {
     try {
